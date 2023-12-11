26758bac2481c046a2d7d29b1a66981b67aa3f7a
Renze Nicolai <renze@rnplus.nl>
2023-12-11 01:54:56 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,142 @@
+#include "ch32v003fun.h"
+#include "i2c_slave.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+// Board revision
+#define HW_REV 1
+
+uint8_t i2c_registers[255] = {0};
+
+uint8_t curr_i2c_registers[sizeof(i2c_registers)] = {0};
+uint8_t prev_i2c_registers[sizeof(i2c_registers)] = {0};
+
+bool i2c_changed = false;
+
+void i2c_callback() {
+    i2c_changed = true;
+}
+
+void control_leds(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e) {
+    GPIOA->CFGLR &= ~((0xf<<(4*1)) | (0xf<<(4*2)));
+    GPIOC->CFGLR &= ~((0xf<<(4*0)) | (0xf<<(4*3)));
+    GPIOD->CFGLR &= ~(0xf<<(4*0));
+
+    if (a > 1) {
+        GPIOD->CFGLR |= (GPIO_Speed_In | GPIO_CNF_IN_FLOATING) << (4 * 0);
+    } else {
+        GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP) << (4 * 0);
+        GPIOD->BSHR  |= 1 << (0 + (a ? 16 : 0));
+    }
+
+    if (b > 1) {
+        GPIOC->CFGLR |= (GPIO_Speed_In | GPIO_CNF_IN_FLOATING) << (4 * 0);
+    } else {
+        GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP) << (4 * 0);
+        GPIOC->BSHR  |= 1 << (0 + (b ? 16 : 0));
+    }
+
+    if (c > 1) {
+        GPIOA->CFGLR |= (GPIO_Speed_In | GPIO_CNF_IN_FLOATING) << (4 * 1);
+    } else {
+        GPIOA->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP) << (4 * 1);
+        GPIOA->BSHR  |= 1 << (1 + (c ? 16 : 0));
+    }
+
+    if (d > 1) {
+        GPIOC->CFGLR |= (GPIO_Speed_In | GPIO_CNF_IN_FLOATING) << (4 * 3);
+    } else {
+        GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP) << (4 * 3);
+        GPIOC->BSHR  |= 1 << (3 + (d ? 16 : 0));
+    }
+
+    if (e > 1) {
+        GPIOA->CFGLR |= (GPIO_Speed_In | GPIO_CNF_IN_FLOATING) << (4 * 2);
+    } else {
+        GPIOA->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP) << (4 * 2);
+        GPIOA->BSHR  |= 1 << (2 + (e ? 16 : 0));
+    }
+}
+
+void leds_off() {
+    control_leds(2,2,2,2,2);
+}
+
+void led_on(uint8_t led) { // Select one specific LED
+    switch(led) {
+        case 1:  return control_leds(2,2,0,2,1); // A
+        case 2:  return control_leds(0,2,1,2,2); // B
+        case 3:  return control_leds(2,2,2,0,1); // D
+        case 4:  return control_leds(2,2,0,1,2); // E
+        case 5:  return control_leds(0,1,2,2,2); // F
+        case 6:  return control_leds(2,0,1,2,2); // G
+        case 7:  return control_leds(2,0,2,2,1); // H
+        case 8:  return control_leds(0,2,2,2,1); // I
+        case 9:  return control_leds(2,0,2,1,2); // K
+        case 10: return control_leds(0,2,2,1,2); // L
+        case 11: return control_leds(1,2,0,2,2); // M
+        case 12: return control_leds(1,0,2,2,2); // N
+        case 13: return control_leds(2,1,2,2,0); // O
+        case 14: return control_leds(2,2,1,2,0); // P
+        case 15: return control_leds(1,2,2,2,0); // R
+        case 16: return control_leds(2,1,0,2,2); // S
+        case 17: return control_leds(2,2,1,0,2); // T
+        case 18: return control_leds(1,2,2,0,2); // V
+        case 19: return control_leds(2,1,2,0,2); // W
+        case 20: return control_leds(2,2,2,1,0); // Y
+        default: return control_leds(2,2,2,2,2); // Off
+    }
+}
+
+void read_buttons(uint8_t* values) {
+    for (uint8_t button = 0; button < 5; button++) {
+        GPIOD->BSHR |= 1 << (6 - button + 16);
+        values[button] = (~(GPIOC->INDR >> 5)) & 0x07; // Read MSWR (bit 0), MSWB (bit 1) and MSWL (bit 2) all at once
+        GPIOD->BSHR |= 1 << (6 - button);
+    }
+}
+
+int main() __attribute__((optimize("O0")));
+int main() {
+    SystemInit();
+
+    // Enable GPIO ports A, C and D
+    RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD;
+
+    // Initialize I2C in peripheral mode on pins PC1 (SDA) and PC2 (SCL)
+    SetupI2CSlave(0x42, 0, i2c_registers, sizeof(i2c_registers), i2c_callback);
+
+    // Configure MSWS1 (PD6), MSWS2 (PD5), MSWS3 (PD4), MSWS4 (PD3), MSWS5 (PD2) for output open-drain mode
+    // and set them all to high
+    for (uint8_t i = 2; i <= 6; i++) {
+        GPIOD->CFGLR &= ~(0xf<<(4*i));
+        GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_OD) << (4 * i);
+        GPIOD->BSHR  |= 1 << i;
+    }
+
+    // Configure MSWL (PC7), MSWB (PC6) and MSWR (PC5) as inputs
+    for (uint8_t i = 5; i <= 7; i++) {
+        GPIOC->CFGLR &= ~(0xf<<(4*i));
+        GPIOC->CFGLR |= (GPIO_Speed_In | GPIO_CNF_IN_FLOATING) << (4 * i);
+    }
+
+    while (1) {
+        if (i2c_changed) {
+            memcpy(curr_i2c_registers, i2c_registers, sizeof(i2c_registers));
+            i2c_changed = false;
+        }
+
+        uint32_t* led_values = (uint32_t*) &i2c_registers[0];
+        for (uint8_t i = 0; i <= 20; i++) {
+            if ((*led_values >> i) & 1) {
+                led_on(i + 1);
+            }
+        }
+        leds_off();
+
+        read_buttons(&i2c_registers[5]);
+
+        memcpy(prev_i2c_registers, curr_i2c_registers, sizeof(i2c_registers));
+    }
+}
